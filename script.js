dabeb5453c2bd6a27cc7583e0d6aaaedbec63538
viswanath <114475166+Viswa143514@users.noreply.github.com>
2023-09-07 18:44:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const panels=document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+    panel.addEventListener('click',() => {
+        removeActivePanels();
+        panel.classList.add('active');
+    })
+})
+function removeActivePanels(){
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+}
